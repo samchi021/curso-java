4be5f43781deeb569223f5e0d5ee79790b9e1697
samchi021 <samchi021@outlook.com>
2024-02-02 22:03:47 -0600

    archivo modificado compila pero no se ejecuta en mi maquina

--- practica2/Holausuarios.java
+++ practica2/Holausuarios.java
@@ -1,6 +1,12 @@
 public class Holausuarios{
+	/*
 	public static void main(String[] args){
 		String mensaje="hola "+ args[0];
-		System.out.printl(mensaje)
+		System.out.println(mensaje);
+	}
+	*/
+	public static void main(String[] args){
+		System.out.println("Hola Mundo");
+		
 	}
 }
